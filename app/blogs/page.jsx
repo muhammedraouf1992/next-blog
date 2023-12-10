3623ec9eea21f851455ef33f23e3ebab8e4882dc
muhammed <muhammedraouf92@gmail.com>
2023-12-10 16:36:16 +0200

    added the blog picture

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import getAllPosts from "../lib/query";
 import Link from "next/link";
+import Image from "next/image";
 
 const AllBlogs = async () => {
   const posts = await getAllPosts();
   const postsArray = posts.nodes;
+  console.log(posts.nodes[0].featuredImage);
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2">
       {postsArray.map((p) => (
         <div key={p.title}>
+          <div>
+            <Image
+              src={p.featuredImage.node.mediaDetails.sizes[1].sourceUrl}
+              width={p.featuredImage.node.mediaDetails.sizes[1].width}
+              height={p.featuredImage.node.mediaDetails.sizes[1].height}
+            />
+          </div>
           <div>
             <Link href={`/blogs/${p.slug}`}>
               <h1 className="text-blue-500 text-2xl  hover:text-blue-700 duration-200">
